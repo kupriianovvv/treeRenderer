9dc5da69538b7731febfca3dbef8cae44fdeafa4
kupriianovvv <kupriianovvv@gmail.com>
2023-08-09 08:05:41 +0300

    getRenderTree

--- src/List.tsx
+++ src/List.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const rawTree = [
+window.rawTree = [
   { id: 1, title: "a", children: [{ id: 4, title: "d", children: [] }] },
   { id: 2, title: "b", children: [] },
   {
@@ -14,12 +14,7 @@ const rawTree = [
   },
 ];
 
-const getFormattedTree = (
-  rawTree,
-  formattedTree = [],
-  parentId = null,
-  depth = 1
-) => {
+window.getFormattedTree = (rawTree, formattedTree = [], parentId = null) => {
   for (const rawTreeNode of rawTree) {
     const { id, title } = rawTreeNode;
     const childrenIds = rawTreeNode.children.reduce((childrenIds, child) => {
@@ -31,20 +26,63 @@ const getFormattedTree = (
       title,
       childrenIds,
       parentId,
-      depth,
-      opened: true,
     };
     formattedTree.push(formattedTreeNode);
     if (childrenIds.length === 0) {
       continue;
     }
-    getFormattedTree(rawTreeNode.children, formattedTree, id, depth + 1);
+    getFormattedTree(rawTreeNode.children, formattedTree, id);
   }
   return formattedTree;
 };
 
+window.getRenderTree = (formattedTree) => {
+  const renderTree = [];
+  for (const parentNode of formattedTree) {
+    {
+      if (!renderTree.includes(parentNode)) {
+        renderTree.push(parentNode);
+      }
+    }
+    if (parentNode.childrenIds.length === 0) {
+      continue;
+    }
+    for (const childNode of formattedTree) {
+      if (parentNode.childrenIds.includes(childNode.id)) {
+        if (!renderTree.includes(childNode)) {
+          renderTree.push(childNode);
+        }
+      }
+    }
+  }
+  return renderTree;
+};
+
 export const List = () => {
-  const [tree, setTree] = useState(getFormattedTree(rawTree));
+  const [tree, setTree] = useState<any[]>(getFormattedTree(rawTree));
+
+  const addChild = (childId, newParentId, oldParentId) => {
+    setTree((prevTree) =>
+      prevTree.map((treeNode) => {
+        if (
+          treeNode.id !== childId &&
+          treeNode.id !== newParentId &&
+          treeNode.id !== oldParentId
+        ) {
+          return treeNode;
+        } else if (treeNode.id === childId) {
+          return { ...treeNode, parentId: newParentId };
+        } else if (treeNode.id === newParentId) {
+          return { ...treeNode, childrenIds: [...childrenIds, childId] };
+        } else if (treeNode.id === oldParentId) {
+          return {
+            ...treeNode,
+            childrenIds: childrenIds.filter((id) => id !== childId),
+          };
+        }
+      })
+    );
+  };
 
   return (
     <ul>
