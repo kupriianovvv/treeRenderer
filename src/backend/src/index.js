eb4c76215b8bbfe051e76dd099c0226ee6a41a80
kupriianovvv <kupriianovvv@gmail.com>
2023-10-21 17:44:10 +0300

    get children handler

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -1,8 +1,37 @@
 import express from "express";
 import cors from "cors";
+import { current } from "immer";
 
 const app = express();
 const port = 8080;
+
+const getRootNodes = (rawTree) => {
+  const rootNodes = rawTree.reduce((rootNodes, currentNode) => {
+    rootNodes.push({
+      id: currentNode.id,
+      title: currentNode.title,
+      hasChildren: currentNode.children.length > 0,
+      children: [],
+    });
+    return rootNodes;
+  }, []);
+  return rootNodes;
+};
+
+const getChildrenNodes = (rawTree, id) => {
+  const childrenNodes = [];
+  for (const node of rawTree) {
+    if (node.id !== id) {
+      if (node.children.length === 0) {
+        continue;
+      }
+      getChildrenNodes(node.children, id);
+    } else {
+      return node.children.map((child) => ({ ...child, children: [] }));
+    }
+  }
+  return childrenNodes;
+};
 
 const rawTree = [
   { id: 1, title: "a", children: [{ id: 4, title: "d", children: [] }] },
@@ -31,7 +60,12 @@ const rawTree = [
 app.use(cors());
 
 app.get("/tree", (req, res) => {
-  res.send(rawTree);
+  res.send(getRootNodes(rawTree));
+});
+
+app.get("/tree/:parentId", (req, res) => {
+  const response = getChildrenNodes(rawTree, +req.params.parentId);
+  res.send(response);
 });
 
 app.listen(port, () => {
